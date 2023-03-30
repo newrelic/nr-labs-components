a83882d6699d3b429ef18570ef412ea3180091c4
Amit Yathirajadasan <ayathirajadasan@newrelic.com>
2023-03-30 13:19:19 -0700

    feat: edit in place component

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export { default as MultiSelect } from './multi-select';
 export { default as HelpModal } from './HelpModal';
 export { default as Messages } from './Messages';
+export { default as EditInPlace } from './edit-in-place';
