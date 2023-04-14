2bf7eed628fb98b826b41fe15126ecf9d0e372f6
Amit Yathirajadasan <ayathirajadasan@newrelic.com>
2023-04-14 12:05:44 -0700

    fix: update patterns regex to support newer keywords and functions

--- src/components/nrql-editor/patterns.js
+++ src/components/nrql-editor/patterns.js
@@ -14,12 +14,12 @@ const patterns = [
   {
     name: 'keyword',
     regex:
-      /\b(?:SELECT|FROM|SHOW EVENT TYPES|WHERE|AS|FACET|FACET CASES|LIMIT|SINCE|UNTIL|WITH TIMEZONE|COMPARE WITH|TIMESERIES|EXTRAPOLATE|AUTO|MAX)\b/gi,
+      /\b(?:SELECT|FROM|SHOW EVENT TYPES|WHERE|AS|FACET|FACET CASES|LIMIT|SINCE|UNTIL|WITH TIMEZONE|COMPARE WITH|TIMESERIES|EXTRAPOLATE|AUTO|MAX|IN|ORDER BY|LIMIT|OFFSET|WITH|SLIDE BY|WITH METRIC_FORMAT)\b/gi,
   },
   {
     name: 'function',
     regex:
-      /(apdex|average|buckets|count|eventType|filter|funnel|histogram|keyset|latest|max|median|min|percentage|percentile|rate|stddev|sum|uniqueCount|uniques)(?=\()/gi,
+      /(apdex|average|buckets|count|eventType|filter|funnel|histogram|keyset|latest|max|median|min|percentage|percentile|rate|stddev|sum|uniqueCount|uniques|aggregationendtime|bucketPercentile|cardinality|dateOf|derivative|dimensions|include|exclude|latestrate|minuteOf|mod|montnOf|quarterOf|predictLinear|round|stdvar|weekOf|yearOf|accountId|aparse|concat|earliest|eventType|getField|if|length|lower|position|string|substring|upper|abs|clamp_max|clamp_min|exp|pow|sqrt)(?=\()/gi,
   },
 ];
 
