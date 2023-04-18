bc6d8c44226edaa7b6a9965c7702b576891911b1
shahram <shahram@newrelic.com>
2023-04-18 14:38:24 -0700

    refactor: style changes

--- src/components/simple-billboard/index.js
+++ src/components/simple-billboard/index.js
@@ -74,7 +74,7 @@ const SimpleBillboard = ({ metric, prefix, suffix, className, style }) => {
         <div
           className={`${styles['metric-content--colorblack']} ${
             styles['metric-content--size1_4em']
-          } ${styles['metric-content--weight900']} ${className || ''}`}
+          } ${styles['metric-content--weight700']} ${className || ''}`}
           style={style || {}}
         >
           <div className={styles['metric']}>
